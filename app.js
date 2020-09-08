a7c56442991fc8b064e23666a369c69ea0bc229e
Paul McQuade <paulmcquad@gmail.com>
2020-09-08 20:56:34 +0100

    Added Button 2 Click
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-console.log('It works')
+console.log("It works");
 
 function createCard() {
-    document.querySelector('#card').innerHTML = ('something here 2')
-    console.log('from createcard')
+  document.querySelector("#card").innerHTML = "something here 2";
+  console.log("from createcard");
 }
 
+function createCard() {
+  document.querySelector("#card").innerHTML = "something here 3";
+  console.log("from createcard");
+}
 
-document.querySelector('.btn').addEventListener('mouseenter', createCard)
+document.querySelector(".btn").addEventListener("mouseenter", createCard);
 
+document.querySelector(".btn2").addEventListener("click", createCard);
